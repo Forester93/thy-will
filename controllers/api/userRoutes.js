6d3afaa3dbc40c7e550b1ec8dbd8f48e97c0991e
ZhouTian0716 <egglord0716@gmail.com>
2021-06-06 09:58:14 +1000

    welcome and profile page draft version ready

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,21 +1,6 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
-//localhost:3001/api/users/
-// router.post('/', async (req, res) => {
-//   try {
-//     const userData = await User.create(req.body);
-
-//     req.session.save(() => {
-//       req.session.user_id = userData.id;
-//       req.session.logged_in = true;
-
-//       res.status(200).json(userData);
-//     });
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
 
 router.post("/", async (req, res) => {
   try {
