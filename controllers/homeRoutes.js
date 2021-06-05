a00a6ba1addb0e77ca462a44c9b6c2a0699511cf
ZhouTian0716 <egglord0716@gmail.com>
2021-06-05 17:39:05 +1000

    route issues pending

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,33 +2,32 @@ const router = require("express").Router();
 const { User } = require("../models");
 const withAuth = require("../utils/auth");
 
-router.get("/", withAuth, async (req, res) => {
+// route to welcome page
+router.get("/", async (req, res) => {
+  res.render("home");
+});
+
+//route to profile
+router.get("/profile", withAuth, async (req, res) => {
   try {
-    const userData = await User.findAll({
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ["password"] },
-      order: [["first_name", "ASC"]],
+    // join other table data here later
     });
 
-    const users = userData.map((project) => project.get({ plain: true }));
-    console.log(users);
-    let user = users[1];
-
+    const user = userData.get({ plain: true });
+    
     res.render("userpage", {
-      user,
-      logged_in: req.session.logged_in,
+      ...user,
+      logged_in: true
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get("/login", (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect("/");
-    return;
-  }
 
-  res.render("login");
-});
+
 
 module.exports = router;
--- public/assets/scripts/logout.js
+++ public/assets/scripts/logout.js
@@ -5,7 +5,7 @@ const logout = async () => {
   });
 
   if (response.ok) {
-    document.location.replace('/login');
+    document.location.replace('/');
   } else {
     alert(response.statusText);
   }
