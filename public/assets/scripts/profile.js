f4a8eddffce0aa0506224c722dee8d3a69ae5284
Marco <markforester93@gmail.com>
2021-06-08 19:05:28 +1000

    Modal Shows Data for Beneficiary

--- public/assets/scripts/profile.js
+++ public/assets/scripts/profile.js
@@ -1,12 +1,12 @@
-const beneficiariesList = document.getElementById("part-1");
-let beneficiaryData = [];
-fetch("/api/users/data/1")
-  .then((response) => {
-    return response.json();
-  })
-  .then((response) => {
-    console.log(response);
-  });
+// const beneficiariesList = document.getElementById("part-1");
+// let beneficiaryData = [];
+// fetch("/api/users/data/1")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((response) => {
+//     console.log(response);
+//   });
 
 // ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ All page rendering relevant codes ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 
@@ -18,28 +18,39 @@ fetch("/api/users/data/1")
 //   // $('beneficiary-add').val().trim();
 
 // }
-$('#addBeneficiary').on('click', addBenificiary);
+$("#addBeneficiary").on("click", addBenificiary);
 function addBenificiary() {
-  var newName = $('#beneficiaryName').val();
+  var newName = $("#beneficiaryName").val();
   // var newDOB = $('#beneficiaryDOB').val();
   // var newRelationship = $('#beneficiaryRelation').val();
   // var newAddress = $('#beneficiaryAddress').val();
-  var newNameEl = $('<button>').text(newName);
+  var newNameEl = $("<button>").text(newName);
   artEl.append(newNameEl);
-  
-  //Backend delete pending
 
+  //Backend delete pending
 }
 
-$('.fa-times').on('click', deleteBenificiary);
+$(".fa-times").on("click", deleteBenificiary);
 function deleteBenificiary(event) {
   event.stopPropagation();
   $(event.target).parent().remove();
   //Backend delete pending
-
 }
-  
 
+let beneficiaryName = $("#beneficiaryName");
+let beneficiaryDOB = $("#beneficiaryDOB");
+let beneficiaryAddress = $("#beneficiaryAddress");
+let beneficiaryRelation = $("#beneficiaryRelation");
+
+$(".beneficiaryBtn").on("mouseover", (event) => {
+  //   event.stopPropagation();
+  let benificiaryBtn = $(event.target);
+  let beneficiaryObject = JSON.parse(benificiaryBtn.attr("data"));
+  beneficiaryName.val(beneficiaryObject.name);
+  beneficiaryDOB.val(beneficiaryObject.DOB);
+  beneficiaryRelation.val(beneficiaryObject.relationship);
+  beneficiaryAddress.val(beneficiaryObject.address);
+});
 
 // ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ All page rendering relevant codes ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 
