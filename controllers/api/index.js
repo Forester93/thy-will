ecfadba7cb52094cd20ec4ed360d9b795a267e18
choonchy <57583113+choonchy@users.noreply.github.com>
2021-06-08 19:32:24 +1000

    update structure

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const crudRoutes = require('./crud');
+const updateRoutes = require('./update');
 const accountRoutes = require('./accountRoutes');
 const pdfRoutes = require('./pdfRoutes');
 const userRoutes = require('./userRoutes');
 
-router.use('/crud', crudRoutes);
+router.use('/update', updateRoutes);
 router.use('/accounts', accountRoutes);
 router.use('/pdf', pdfRoutes);
 router.use('/users', userRoutes);
