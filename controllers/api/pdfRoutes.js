8f5a13635cf140bc8d0dd7270a6eafb9fac20415
choonchy <57583113+choonchy@users.noreply.github.com>
2021-06-08 21:58:22 +1000

    updating beneficiaries works

--- controllers/api/pdfRoutes.js
+++ controllers/api/pdfRoutes.js
@@ -269,7 +269,7 @@ router.get('/:id', withAuth, async (req, res) => {
 
 			console.log('done');
 		} catch (err) {
-			res.status(403).json(err);
+			res.status(500).json(err);
 		}
 	} else {
 		console.log('redirecting');
--- controllers/api/update/beneficiaryRoutes.js
+++ controllers/api/update/beneficiaryRoutes.js
@@ -2,10 +2,39 @@ const router = require('express').Router();
 const withAuth = require('../../../utils/auth');
 const { User, Beneficiary } = require('../../../models');
 
-router.get('/:id', async (req, res) => {
-	const beneficiaryUpdate = Beneficiary.findByPk(req.params.id);
+router.put('/:id', async (req, res) => {
+	Beneficiary.update(
+		{
+			name: req.body.name,
+			address: req.body.address,
+		},
+		{
+			where: {
+				id: req.params.id,
+			},
+		}
+	)
+		.then((update) => {
+			res.json(update);
 
-	console.log(beneficiaryUpdate);
+			res.redirect('/');
+		})
+		.catch((err) => {
+			console.log(err);
+			res.json(err);
+		});
+});
+
+router.get('/', async (req, res) => {
+	try {
+		const beneficiaryUpdate = await Beneficiary.findAll();
+
+		console.log(beneficiaryUpdate);
+
+		res.redirect('../../profile');
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 module.exports = router;
