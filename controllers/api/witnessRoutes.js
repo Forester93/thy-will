786ca37c8dcee71b6b5ac48d2156e25628e8e378
ZhouTian0716 <egglord0716@gmail.com>
2021-06-09 00:23:24 +1000

    commit for pull main

---
+++ controllers/api/witnessRoutes.js
@@ -0,0 +1,47 @@
+const router = require("express").Router();
+const { Witness } = require("../../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const witnessData = await Witness.findAll({});
+    res.status(200).json(witnessData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const categoryData = await Beneficiary.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "No Category found with this id!" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/create", async (req, res) => {
+  // create a new account
+  try {
+    const accountInfo = await Account.create(req.body);
+    let account = accountInfo.get({ plain: true });
+    res.status(200).json("Account created!");
+    req.session.save(() => {
+      req.session.account_id = account.id;
+      req.session.logged_in = true;
+      res.json({ account: account, message: "You are now logged in!" });
+    });
+    res.redirect("/");
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+module.exports = router;
